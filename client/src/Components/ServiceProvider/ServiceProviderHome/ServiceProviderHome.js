32724014100e0522f8960a2c05b681aca2a12ab5
Muhammad Imran Rafiq <mohammedimranrafique@gmail.com>
2024-08-18 21:31:27 +0500

    adding service provider sign ui authentication code

--- client/src/Components/ServiceProvider/ServiceProviderHome/ServiceProviderHome.js
+++ client/src/Components/ServiceProvider/ServiceProviderHome/ServiceProviderHome.js
@@ -1,7 +1,25 @@
-import React from "react";
-
+import React, { useEffect, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { handleShowSuccessToast } from "../../ToastMessages/ToastMessage";
+import { Toaster } from "react-hot-toast";
 const ServiceProviderHome = () => {
-  return <div>ServiceProviderHome</div>;
+  const location = useLocation();
+  const navigate = useNavigate();
+  const hasShownToast = useRef(false);
+  const myMessage = location.state.message || null;
+  useEffect(() => {
+    if (myMessage && !hasShownToast.current) {
+      handleShowSuccessToast(myMessage);
+      hasShownToast.current = true;
+      navigate(location.pathname, { replace: true, state: {} });
+    }
+  }, [myMessage, navigate, location.pathname]);
+  return (
+    <>
+      <Toaster />
+      <div>ServiceProviderHome</div>;
+    </>
+  );
 };
 
 export default ServiceProviderHome;
